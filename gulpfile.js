4cb02d0b7a49c2afe20c318e020ab057ca80d345
Julien Liabeuf <julien@liabeuf.fr>
2016-04-10 20:03:46 +0700

    Add Gulp Surge

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     path = require('path'),
     cleanCSS = require('gulp-clean-css'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    surge = require('gulp-surge');
 
 gulp.task('default', function () {
     // place code for your default task here
@@ -17,6 +18,13 @@ gulp.task('minify-css', function () {
     return gulp.src('./themes/helios/static/css/main.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./themes/helios/static/css/'));
+});
+
+gulp.task('deploy', [], function () {
+    return surge({
+        project: './public',         // Path to your static build directory
+        domain: 'jacquesliabeuf.surge.sh'  // Your domain or Surge subdomain
+    })
 });
 
 gulp.task('css', ['sass', 'minify-css']);
