f3b0795d53b57b6f13418f52e2b4c616c7f8a871
Julien Liabeuf <julien@liabeuf.fr>
2016-05-29 13:26:48 +0700

    Concatenate & minify scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp'),
     path = require('path'),
     cleanCSS = require('gulp-clean-css'),
     sass = require('gulp-sass'),
-    surge = require('gulp-surge');
+    surge = require('gulp-surge'),
+    concat = require('gulp-concat-util'),
+    uglify = require('gulp-uglify'),
+    rename = require("gulp-rename");
 
 gulp.task('default', function () {
     // place code for your default task here
@@ -18,6 +21,30 @@ gulp.task('minify-css', function () {
     return gulp.src('./themes/helios/static/css/main.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./themes/helios/static/css/'));
+});
+
+gulp.task('js-minify', function () {
+    return gulp.src(['./themes/helios/static/js/main.js', './themes/helios/static/js/util.js'])
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./themes/helios/static/js'));
+});
+
+gulp.task('js-concat', function () {
+    gulp.src(
+        ['./themes/helios/static/js/jquery.dropotron.min.js',
+            './themes/helios/static/js/jquery.scrolly.min.js',
+            './themes/helios/static/js/jquery.onvisible.min.js',
+            './themes/helios/static/js/skel.min.js',
+            './themes/helios/static/js/util.min.js',
+            './themes/helios/static/js/main.min.js'
+        ])
+        .pipe(concat('dist.js'))
+        .pipe(concat.header('// file: <%= file.path %>\n'))
+        .pipe(concat.footer('\n// end\n'))
+        .pipe(gulp.dest('./themes/helios/static/js'));
 });
 
 gulp.task('deploy', [], function () {
